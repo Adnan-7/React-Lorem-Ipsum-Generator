d98b45358eb56f68f3ed960491d7a2247ddcdb1d
Adnan Khan <adnan7.zain@gmail.com>
2021-11-22 12:07:27 +0500

    add the lorem ipsum generate functionality

--- src/App.js
+++ src/App.js
@@ -4,26 +4,41 @@ import data from './data';
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  };
+
   return (
     <section className='section-center'>
       <h3>tired of boring lorem ipsum</h3>
-      <form className='lorem-form'>
+      <form className='lorem-form' onSubmit={handleSubmit}>
         <label htmlFor='count'>paragraphs</label>
-        <input type='number' name='count' id='count' />
+        <input
+          type='number'
+          name='count'
+          id='count'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
         <button type='submit' className='btn'>
           generate
         </button>
       </form>
 
       <article>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptas
-          incidunt, a eius tenetur adipisci delectus culpa totam neque unde.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptas
-          incidunt, a eius tenetur adipisci delectus culpa totam neque unde.
-        </p>
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
       </article>
     </section>
   );
