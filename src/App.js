d515386b121d85f0215619897b8a032da9dc1718
Adnan Khan <adnan7.zain@gmail.com>
2021-11-22 11:46:21 +0500

    design a basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 
 function App() {
-  return <div>hello</div>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  return (
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum</h3>
+      <form className='lorem-form'>
+        <label htmlFor='count'>paragraphs</label>
+        <input type='number' name='count' id='count' />
+        <button type='submit' className='btn'>
+          generate
+        </button>
+      </form>
+
+      <article>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptas
+          incidunt, a eius tenetur adipisci delectus culpa totam neque unde.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptas
+          incidunt, a eius tenetur adipisci delectus culpa totam neque unde.
+        </p>
+      </article>
+    </section>
+  );
 }
 
 export default App;
